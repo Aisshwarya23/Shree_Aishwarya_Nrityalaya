584e05c7eaae9802032fa4475538754f8eee7392
Aishwarya C <165939227+Aisshwarya23@users.noreply.github.com>
2026-03-04 00:58:15 +0530

    Update database password in server.js

--- dance-backend/server.js
+++ dance-backend/server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 const db = mysql.createConnection({
   host:     "localhost",
   user:     "root",
-  password: "aish@123",
+  password: "yourpassword",
   database: "dance_academy",
 });
 
